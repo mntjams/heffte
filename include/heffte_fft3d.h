f2bb2c16b54d398b97097df9257330cfc024d5d8
mntjams <matouszatloukal@seznam.cz>
2026-07-06 07:38:35 +0000

    Merge two identical blocks

--- include/heffte_fft3d.h
+++ include/heffte_fft3d.h
@@ -640,9 +640,8 @@ private:
                 scale_factor /= 8.0;
             }else if (std::is_same<backend_tag, backend::fftw_cos1>::value) {
                 scale_factor = 1.0 / (8.0 * (plan.fft_sizes[0] - 1) * (plan.fft_sizes[1] - 1) * (plan.fft_sizes[2] - 1));
-            }else if (std::is_same<backend_tag, backend::fftw_sin1>::value) {
-                scale_factor = 1.0 / (8.0 * (plan.fft_sizes[0] + 1) * (plan.fft_sizes[1] + 1) * (plan.fft_sizes[2] + 1));
-            }else if(std::is_same<backend_tag, backend::cufft_sin1>::value){
+            }else if (std::is_same<backend_tag, backend::fftw_sin1>::value or
+                      std::is_same<backend_tag, backend::cufft_sin1>::value) {
                 scale_factor = 1.0 / (8.0 * (plan.fft_sizes[0] + 1) * (plan.fft_sizes[1] + 1) * (plan.fft_sizes[2] + 1));
             }else if(std::is_same<backend_tag, backend::cufft_cos1>::value or
                      std::is_same<backend_tag, backend::rocfft_cos1>::value or
